d3741ecaec82ac514a40702f80f9ecd3879a04db
caifan88 <47955699+caifan88@users.noreply.github.com>
2019-04-18 14:24:08 +0800

    初次修改

--- src/main/java/com/caifan/one/OneApplication.java
+++ src/main/java/com/caifan/one/OneApplication.java
@@ -1,9 +1,11 @@
 package com.caifan.one;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.caifan.one")
+@MapperScan("com.caifan.one.mapper")
 public class OneApplication {
 
     public static void main(String[] args) {
